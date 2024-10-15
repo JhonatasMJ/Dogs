2fb7bf825f4129e0b6d13cc7934d8af5ec51fdb1
JhonatasMJ <jhonatasjhmj@gmail.com>
2024-10-15 13:40:02 -0300

    Estrutura Inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import "./App.css";
-
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
 
 const App = () => {
   return (
     <div>
-      
-  </div>
-  ) 
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
